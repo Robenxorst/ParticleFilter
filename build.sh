c30dafeba5530728396e2da8c759624a61c64f63
NIKOLAY POLOVNIKOV <82658563+Robenxorst@users.noreply.github.com>
2022-02-19 14:32:15 +0300

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to build all components from scratch, using the maximum available CPU power
+#
+# Given parameters are passed over to CMake.
+# Examples:
+#    * ./build_all.sh -DCMAKE_BUILD_TYPE=Debug
+#    * ./build_all.sh VERBOSE=1
+#
+# Written by Tiffany Huang, 12/14/2016
+#
+
+# Go into the directory where this bash script is contained.
+#cd `dirname $0`
+
+# Unzipping the package
+cd data
+tar -xvf observation.tar.gz
+cd ..
+
+# Compile code.
+mkdir -p build
+cd build
+cmake ..
+make -j `nproc` $*
---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script to clean the tree from all compiled files.
+# You can rebuild them afterwards using "build.sh".
+#
+# Written by Tiffany Huang, 12/14/2016
+#
+
+# Remove the dedicated output directories
+#cd `dirname $0`
+
+cd data
+rm -rf observation
+cd ..
+rm -rf build
+
+# We're done!
+echo Cleaned up the project!
